f60d160995fb2a78476c38aa795a28d95ce0b746
rajcodes168h <raj19all@gmail.com>
2024-02-17 21:21:45 +0530

    cyclic deploymet

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 
-require('dotenv').config()
- 
-const  connection = mongoose.connect(process.env.mongourl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  
-
- 
-module.exports = {
-    connection
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false, // Disable buffering
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+    
+    // Now you can execute queries
+    // Example:
+    // const user = await User.findOne({ username: 'example' });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
+
+connectToDatabase();
