35e3fc2a1d9d198b68466f0214da0cb669884513
rajcodes168h <raj19all@gmail.com>
2024-02-17 21:05:35 +0530

    cyclic deploymet

--- config/db.js
+++ config/db.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 
 require('dotenv').config()
 
-const connection = mongoose.connect(process.env.mongourl);
+ 
+const connection = mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
+ 
 module.exports = {
     connection
 }
