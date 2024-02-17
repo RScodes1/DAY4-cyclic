8669b73a368944999ef916eea91432d5c54ae8d8
rajcodes168h <raj19all@gmail.com>
2024-02-17 21:16:25 +0530

    cyclic deploymet

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
  
 const connection = mongoose.connect(process.env.mongourl, {
     bufferCommands: false, // Disable buffering
-    buffertimeoutms: 30000, // Increase timeout to 30 seconds
+    // buffertimeoutms: 30000, // Increase timeout to 30 seconds
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
