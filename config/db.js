34a35863b1c09461eb2800558cbb73ecb7931cbf
rajcodes168h <raj19all@gmail.com>
2024-02-17 21:12:37 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config()
  
-const connection = mongoose.connect(process.env.mongourl, {
+const connection = await mongoose.connect(process.env.mongourl, {
     bufferCommands: false, // Disable buffering
     buffertimeoutms: 30000, // Increase timeout to 30 seconds
     useNewUrlParser: true,
