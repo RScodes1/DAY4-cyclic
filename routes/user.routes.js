d1e21c828cd2c6d3709f39e68fe61dc02824b43c
rajcodes168h <raj19all@gmail.com>
2024-02-18 13:53:45 +0530

    swaggerodcs

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -85,26 +85,45 @@ userRouter.post('/register', async(req, res)=> {
  * @swagger
  * /users/register:
  *  post:
- *      summary: registers the new user details data to database
- *      tags: [Users]
- *      requestBody: 
- *          required: true
- *          content:
- *              application/json:
- *              schema:
- *                  $ref: "#/components/schemas/User"
- *      responses:
- *          200:
- *              description: To add a new user to list of all the users
- *              content:
- *                  application/json:
- *                      schema:                
- *                          item:
- *                              $ref:"#/components/schemas/User"
- *          400: 
- *               description: internal server error
- *          500:
- *               description: Internal server error
+ *    summary: Register a new user
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - username
+ *              - email
+ *              - password
+ *            properties:
+ *              username:
+ *                type: string
+ *                description: The username of the user
+ *              email:
+ *                type: string
+ *                format: email
+ *                description: The email address of the user
+ *              password:
+ *                type: string
+ *                format: password
+ *                description: The password of the user
+ *    responses:
+ *      200:
+ *        description: User registered successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                msg:
+ *                  type: string
+ *                  description: Message confirming the registration
+ *      401:
+ *        description: Unauthorized - User already exists
+ *      500:
+ *        description: Internal server error
  */
 
 
